0fa09ebe4c49c08d6b1037b8f9b1fe1e90d3adb1
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-01-01 00:34:15 -0500

    Adds (commented-out) some code that may find more info about the packages.

--- problems.rb
+++ problems.rb
@@ -25,14 +25,38 @@ $problems.keys.sort.each do |key|
 		.uniq { |p| p["name"] }
 		.sort { |a, b| a["name"].downcase <=> b["name"].downcase }
 		.map do |package|
-		# FIXME : This does not map to an attribute name, nor to a nixos file.
-		[
-			" * [ ]",
-			package["name"],
-			package["homepage"] ? "(#{package["homepage"]})" : "",
-			package["new_homepage"] ? " → (#{package["new_homepage"]})" : "",
-		].join(" ")
-	end.join("\n")
+			pkgs = NixOS::find_homepage(package["homepage"])
+
+			attr_names = pkgs.map { |attr, pkg| attr }.join(", ")
+			# FIXME : This does not map to an attribute name, nor to a nixos file.
+			main_line = [
+				" * [ ]",
+				package["name"],
+				# Not great
+				#(attr_names and attr_names.length > 0) ? "`#{attr_names}`" : "",
+				package["homepage"] ? "(#{package["homepage"]})" : "",
+				package["new_homepage"] ? " → (#{package["new_homepage"]})" : "",
+			].join(" ")
+			sub_line = ""
+			
+			# Not great either
+			#if pkgs and pkgs.length > 0 then
+			#	sub_line = "\n" + 
+			#		pkgs.map do |attr, pkg|
+			#			position = pkg["meta"]["position"]
+			#			if position then
+			#				file = position.split(":").first
+			#				line = position.split(":").last
+			#				link = "https://github.com/NixOS/nixpkgs/blob/#{NixOS::COMMIT}/#{file}#L#{line}"
+			#				link = "[#{position}](#{link})"
+			#				"     * " + link
+			#			end
+			#		end
+			#		.uniq
+			#		.join("\n")
+			#end
+			main_line + sub_line
+		end.join("\n")
 
 	puts <<~EOF
 	## Problem: #{problem}
